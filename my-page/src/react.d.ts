15ec5c288a5f92dc4482d987c51f77b2d6bb3c7f
Sergiy Poddubka <poseal@gmail.com>
2024-11-07 04:19:01 +0200

    remove odd import

--- my-page/src/react.d.ts
+++ my-page/src/react.d.ts
@@ -1,6 +1,4 @@
 
-import { AriaAttributes, DOMAttributes } from 'react' // not needed if skipLibCheck = true
-
 declare module 'react' {
     interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
         className?: string
